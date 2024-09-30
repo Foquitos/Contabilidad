85cfb84ca798420e468a56e4c0a03d78b821aba7
Ignacio <nacholin04@gmail.com>
2024-09-30 14:44:22 -0300

    Modulos ya no usados

--- Modulos.py
+++ Modulos.py
@@ -2,8 +2,8 @@ import pandas as pd
 
 def Marca_temporal_a_fecha(df):
     #* Relleno la columna fecha por el dia de creacion en caso de no estar la fecha
-    df['Marca temporal'] = pd.to_datetime(df['Marca temporal'],format= '%d/%m/%Y %H:%M:%S')
-    df['Fecha'] = pd.to_datetime(df['Fecha'], format= '%d/%m/%Y')
-    df['Fecha'] = df['Fecha'].fillna(df['Marca temporal'])
+    df['marca_temporal'] = pd.to_datetime(df['marca_temporal'],format= '%d/%m/%Y %H:%M:%S')
+    df['fecha'] = pd.to_datetime(df['marca_temporal'], format= '%d/%m/%Y').dt.date
+    df['fecha'] = df['fecha'].fillna(df['marca_temporal'].dt.date)
     
     return df
